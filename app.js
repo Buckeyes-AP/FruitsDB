f7a00f8f02742f2f8fe5bef63bb199828532b07d
Alex Poirier <alexmatthewpoirier@gmail.com>
2023-01-09 17:15:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//jshint esversion:6
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
+
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    rating: Number,
+    review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit."
+});
+
+const peopleSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", peopleSchema);
+
+const person = new Person ({
+    name: "John",
+    age: 37
+});
+
+
+//fruit.save();
+person.save();
+
+const kiwi = new Fruit({
+    name: "Kiwi",
+    score: 10,
+    review: "The best fruit!"
+});
+
+const orange = new Fruit({
+    name: "Orange",
+    score: 4,
+    review: "Too sour for me"
+});
+
+const banana = new Fruit({
+    name: "Banana", 
+    score: 3,
+    review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+    if (err) {
+        consloe.log(err);
+    } else {
+        console.log("Successfully saved all the fruits to fruitsDB");
+    }
+});
